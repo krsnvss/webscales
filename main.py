188374a36879110e6978567af0390dd3e212bd6d
krsnvs <krsnvss@gmail.com>
2018-09-16 22:31:27 +0300

    New web page showing current scale value

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_restful import abort, Api, reqparse, Resource
 from configparser import ConfigParser
 from datetime import datetime
 from serial import Serial
+from random import randint
 
 app = Flask(__name__)
 api = Api(app)
@@ -21,6 +22,19 @@ def scribe(_str):
         ))
 
 
+def get_value():
+    serialport = Serial(
+        port=scales_port,
+        baudrate=scales_baudrate,
+        bytesize=8,
+        parity='N'
+    )
+    with serialport as rs:
+        weight = rs.readline()
+        scribe("Got scales value {}".format(str(weight)))
+    return weight
+
+
 class ScalesApi(Resource):
 
     def __init__(self):
@@ -28,17 +42,8 @@ class ScalesApi(Resource):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument('scales', type=str)
         self.params = self.parser.parse_args()
-        self.rs = Serial(
-            port=scales_port,
-            baudrate=scales_baudrate,
-            bytesize=8,
-            parity='N'
-        )
 
     def get(self):
-        with self.rs as rs:
-            self.weight = rs.readline()
-        scribe("Got scales value {}".format(str(self.weight)))
         self.errors = ''
         return jsonify({
             "scales": scales_name,
@@ -49,6 +54,13 @@ class ScalesApi(Resource):
 
 
 api.add_resource(ScalesApi, "/weight")
+
+
+@app.route('/')
+def index():
+    value = randint(1000, 2000)
+    return render_template('index.html', value=value)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
